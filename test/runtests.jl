756a948f37ad54b25551414e127eee2491be79d3
Jan Hönig <jan.hoenig@fau.de>
2018-03-19 17:11:54 +0100

    work in progress

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,10 @@ using Brainfuck
 
 # Use the @DIR macro
 
-buffer = IOBuffer()
-brainfuck_interpret("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.)", stream_out = buffer)
-print(buffer)
+buffer = PipeBuffer()
+brainfuck_interpret("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.", stream_out = buffer)
+s = readstring(buffer)
+print(s)
+if s != "Hello World!\n"
+    print("help")
+end
